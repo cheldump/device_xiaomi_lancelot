aa6d6a6dd12b4c4f9de6ee52601a1e1676371cce
chrisl7 <wandersonrodriguesf1@gmail.com>
2022-09-24 11:12:29 -0400

    lancelot: parts: Import XiaomiParts and sepolicy rules from old tree
    [1] - https://github.com/CHRISL7/android_device_xiaomi_spes/tree/topaz-old
    
    Change-Id: I726d09a6ea5a073fed09d59deb1d465548534477
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -117,6 +117,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libshim_showlogo
 
+# Device Settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/parts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-parts.xml
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
