261621b7d0573b1456ae32eca486361b758b38d3
Bruno Martins <bgcngm@gmail.com>
2024-06-19 08:39:59 +0530

    lancelot: Use common libmtkperf_client and power-libperfmgr
    Change-Id: I38bf7ccbe7d277fc2ae44a04fdf0df19d2112355
    Signed-off-by: hipexscape <abhinav.115260@gmail.com>

--- device.mk
+++ device.mk
@@ -308,7 +308,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr
+    android.hardware.power-service.lineage-libperfmgr
 
 PRODUCT_PACKAGES += \
     libmtkperf_client_vendor \
@@ -441,6 +441,8 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/mediatek \
     hardware/xiaomi \
+    hardware/mediatek/libmtkperf_client \
+    hardware/lineage/interfaces/power-libperfmgr \
     $(LOCAL_PATH)
 
 # Thermal
